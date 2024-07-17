fd1414e0541e0ccb3744706a7a22c4ba3be120da
SChansuk <schnond.work@gmail.com>
2024-07-17 20:50:08 +0700

    Create bruteforce.cpp

---
+++ list/twosum/bruteforce.cpp
@@ -0,0 +1,23 @@
+/*
+ * Brute force method
+ * Time complexity: O(n^2)
+ * Space complexity: O(1)
+ */
+class Solution 
+{
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) 
+        {
+        int n = nums.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) 
+            {
+                if (nums[i] + nums[j] == target) 
+                {
+                    return { i, j };
+                }
+            }
+        }
+        return {};
+    }
+};
